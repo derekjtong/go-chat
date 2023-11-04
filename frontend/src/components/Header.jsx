abcb2bd4b759419c3a3b8f4d909a22d4e6845daa
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-04 01:35:05 -0400

    Tailwind CSS Prettier

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="bg-slate-800 p-10 text-white w-full m-0">
+    <div className="m-0 w-full bg-slate-800 p-10 text-white">
       <h2 className="m-0 p-0">Go React Chat</h2>
     </div>
   );
